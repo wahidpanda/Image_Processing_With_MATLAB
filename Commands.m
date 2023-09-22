10ce31c5e82faeaf76ed98e81904b96a314666fb
Wahid <islamoahidul12@gmail.com>
2023-09-22 14:08:14 +0600

    Update and rename NUMPLATE.m to Commands.m

---
+++ Commands.m
@@ -0,0 +1,45 @@
+
+Image Reading and Display:
+
+imread: Read an image from a file.
+imshow: Display an image.
+Image Enhancement:
+
+imadjust: Adjust image intensity values.
+histeq: Perform histogram equalization.
+Image Filtering:
+
+imfilter: Apply a linear filter to an image.
+medfilt2: Apply median filtering to an image.
+Image Thresholding:
+
+graythresh: Compute a global image threshold using Otsu's method.
+imbinarize: Binarize grayscale or color image.
+Image Segmentation:
+
+bwlabel: Label connected components in a binary image.
+regionprops: Compute properties of image regions or connected components.
+Morphological Operations:
+
+imerode, imdilate: Perform erosion and dilation.
+imopen, imclose: Perform opening and closing operations.
+Image Transformation:
+
+imresize: Resize an image.
+imrotate: Rotate an image.
+Geometric Transformations:
+
+affine2d, imwarp: Perform affine transformations.
+Frequency Domain Operations:
+
+fft2, ifft2: Compute 2-D Fourier transform and its inverse.
+Object Detection:
+
+vision.CascadeObjectDetector: Detect objects using pre-trained cascade object detector.
+Image Analysis and Measurements:
+
+regionprops: Compute properties of image regions or connected components.
+imcontour: Display contour plot of image data.
+Deep Learning (requires Deep Learning Toolbox):
+
+Functions and tools related to deep learning for image processing, including training and using convolutional neural networks (CNNs).
--- NUMPLATE.m
+++
@@ -1,7 +0,0 @@
-
-car_plate=imread('D:/BMW.jpg');
-imshow(car_plate);
-ocrResults   = ocr(car_plate); 
-result  = insertObjectAnnotation(car_plate,'rectangle', ...
-                   ocrResults.WordBoundingBoxes, ...
-                   ocrResults.WordConfidences);
